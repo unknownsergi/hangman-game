309e29b49efe60553b963aed9919c49434169278
flame <sergicampsgili@gmail.com>
2023-11-29 21:16:17 +0100

    La letras ahora se muestran en su lugar.

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,27 +1,57 @@
 const container = document.getElementById("app");
 const wordDisplay = document.getElementById("word-display");
 const letters = document.querySelectorAll(".letters");
+const lifes = document.querySelector("#lifes span");
 
-const word = [];
-let secretWord = "HOLA";
+const underscores = [];
+let secretWord = "JIJIJIJA";
+let secretWordArr = secretWord.split("");
+let loop = true;
+lifeCounter = 7;
+lifes.innerHTML = lifeCounter;
 
-function writeLetter(letter) {
-	word.push(letter);
-	const newWord = word.join("");
-	wordDisplay.innerHTML = newWord;
+generateUnderscores(secretWordArr);
+// Agarro el texto de la letra que he pulsado i uso la funcion writeLetter para escribir en la pantalla de muestra
+
+function generateUnderscores(word) {
+	for (let i = 0; i < word.length; i++) {
+		underscores[i] = "_";
+	}
+	console.log(underscores);
+	wordDisplay.innerHTML = underscores.join(" ");
 }
 
+function replaceUnderscores(letter) {
+	for (let i = 0; i < secretWordArr.length; i++) {
+		if (letter == secretWordArr[i]) {
+			underscores[i] = letter;
+			wordDisplay.innerHTML = underscores.join(" ");
+		}
+	}
+}
+
+// CHECK
 function checkLetter(e) {
 	let letter = e.target.innerHTML;
 	if (secretWord.includes(letter)) {
+		replaceUnderscores(letter);
 		e.target.style.background = "green";
-		writeLetter(letter);
+		// writeLetter(letter);
 	} else {
+		deleteLife();
 		e.target.style.background = "red";
 	}
 }
 
-// Agarro el texto de la letra que he pulsado i uso la funcion writeLetter para escribir en la pantalla de muestra
+function deleteLife() {
+	if (lifeCounter == 0) {
+		loop = false;
+	} else {
+		lifeCounter--;
+		lifes.innerHTML = lifeCounter;
+	}
+}
+
 container.addEventListener("click", (e) => {
 	if (e.target.classList.contains("letter")) {
 		checkLetter(e);
